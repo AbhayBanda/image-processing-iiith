02268e5bb96c1572bfff91fd5d0ee6a74dd570c2
AbhayBanda <bandaabhay@gmail.com>
2019-07-20 11:32:08 +0530

    Made changes in UI

--- SRIP/Codes/myScript.js
+++ SRIP/Codes/myScript.js
@@ -1,185 +1,150 @@
-function myFunction(imgs) {
+$(document).ready(function() {
+	$("#selectImage").click(function(){
+		$("#imageGrid").show()
+	});
+});
 
-var expandImg = document.getElementById("expandedImg");
-var expandIPImg = document.getElementById("ip");
-expandImg.src = imgs.src;
-expandIPImg.src = imgs.src;
-expandImg.parentElement.style.display = "block";
-expandIPImg.parentElement.style.display = "block";
-$("#ioHolder").hide();
+$(document).ready(function() {
+	$("#Reset").click(function(){
+		window.location.reload();
+	});
+});
 
-}
-function showDiv() {
-document.getElementById('imageGrid').style.display = 'block';
-}
+
 
 function hideIg() {
 document.getElementById('imageGrid').style.display = 'none';
 }
 
-function hideInst() {
-document.getElementById('instructionCanvas').style.display = 'none';
+function myFunction(imgs) {
+
+var expandImg = document.getElementById("inputImage");
+var finalImg = document.getElementById("finalInput")
+expandImg.src = imgs.src;
+finalImg.src = imgs.src;
+expandImg.parentElement.style.display = "block";
+finalImg.parentElement.style.display = "block";
+$("#io").hide();
 }
 
-function showInst1() {
-document.getElementById('instructionCanvas2').style.display = 'block';
-}
-
-var canvas = document.getElementById('instructionCanvas');
-var ctx = canvas.getContext('2d');
-canvas.style.display = 'block';
-canvas.style.padding.top = "1px";
-canvas.style.padding.left= "1px";
-canvas.style.border= "dashed grey 1px";
-canvas.position = "relative";
-canvas.height = 50;
-ctx.fillStyle = "#c2c2c2";
-ctx.font = "13px Georgia";
-ctx.position = "relative";
-ctx.top = canvas.height/2;
-ctx.fillText("Select appropriate parameters and click on 'Run'.", 10, 25);
-
-var canvas1 = document.getElementById('instructionCanvas2');
-var ctx1 = canvas1.getContext('2d');
-canvas1.style.display = "none";
-canvas1.style.padding.top = "1px";
-canvas1.style.padding.left= "1px";
-canvas1.style.border= "dashed grey 1px";
-canvas1.position = "relative";
-canvas1.height = 50;
-ctx1.fillStyle = "#c2c2c2";
-ctx1.font = "13px Georgia";
-ctx1.position = "relative";
-ctx1.top = canvas.height/2;
-ctx1.fillText("Note down the results.", 10, 25);
-
-var c = document.getElementById("inputCanvas");
-c.style.display = 'none' 
-c.style.border = "solid red 2px";
-c.style.position = "absolute";
-c.style.left = "400px";
-c.style.top = "70px";
-c.style.width = "600px";
-c.style.height = "700px";
-
-
-
-$(document).ready(function() {
-	$("#Processing").click(function() {
-		if(!document.getElementById('hsi').checked && !document.getElementById('cmy').checked && !document.getElementById('rgb').checked ){alert("Please Select the Parameters")}
-		else if(document.getElementById('hsi').checked){ $("#applyParam").show(); $('.HSI').show(); $('.CMY').hide();$('.RGB').hide(); $("#ioImages").hide();}
-		else if(document.getElementById('cmy').checked){ $("#applyParam").show(); $('.CMY').show();$('.HSI').hide(); $('.RGB').hide(); $("#ioImages").hide();}
-		else if(document.getElementById('rgb').checked){ $("#applyParam").show(); $('.RGB').show();$('.CMY').hide();$('.HSI').hide();$("#ioImages").hide();}
-		$("#ioHolder").show();
-		$("#ioImages").hide();
-		$("#linear").show();
-		$("#histogram").show();
-
-});
-});
-
-
-$(document).ready(function() {
-	$('#colorSpace').click(function() {
-		$("#ioHolder").hide();
-		$('#applyParam').hide();
-		$("#ioImages").show();
-		$("#linear").hide();
-		$("#histogram").hide();
-		$("#linearfunction").hide();
-		$('#graph').hide();
-});
-});
-
-$(document).ready(function() {
-	$('#selectImg').click(function() {
-		$("#ioHolder").hide();
-		$('#applyParam').hide();
-		$("#ioImages").show();
-});
-});
-
-var canvas2 = document.getElementById("finalInput");
-var ctx2 = canvas2.getContext("2d");
-canvas2.style.border = "solid blue 3px";
-canvas2.style.width = "150px";
-canvas2.style.height = "150px";
-canvas2.style.position = "relative";
-canvas2.style.left = "550px";
-canvas2.style.top = "130px";
-
-
-var canvas3 = document.getElementById("finalOutput");
-var ctx3 = canvas3.getContext("2d");
-canvas3.style.border = "solid blue 3px";
-canvas3.style.width = "150px";
-canvas3.style.height = "150px";
-canvas3.style.position = "relative";
-canvas3.style.left = "450px";
-canvas3.style.top = "130px";
-
-
-document.getElementById("run").onclick = function() {
+$(document).ready(function(){
+	$("#Run").click(function(){
 
 	var c3 = document.getElementById("redCanvas");
 	var ctx3 = c3.getContext("2d");
-	var img = document.getElementById("expandedImg");
+	var img = document.getElementById("inputImage");
 	ctx3.drawImage(img, 0, 0);
 	var imgData = ctx3.getImageData(0, 0, c3.width, c3.height);
 	// invert colors
 	var i;
 	for (i = 0; i < imgData.data.length; i += 4) {
-	imgData.data[i] = 255
+		    imgData.data[i] = imgData.data[i];
+    		imgData.data[i+1] = 0;
+    		imgData.data[i+2] = 0;
 	}
 	ctx3.putImageData(imgData, 0,0);
 
 	var c4 = document.getElementById("greenCanvas");
 	var ctx4 = c4.getContext("2d");
-	var img1 = document.getElementById("expandedImg");
+	var img1 = document.getElementById("inputImage");
 	ctx4.drawImage(img1, 0, 0);
 	var imgData1 = ctx4.getImageData(0, 0, c4.width, c4.height);
 	// invert colors
 	var i;
 	for (i = 0; i < imgData1.data.length; i += 4) {
-	imgData1.data[i + 1] = 255
+			    imgData1.data[i] = 0;
+   				imgData1.data[i+1] = imgData1.data[i + 1];
+    			imgData1.data[i+2] = 0;
 	}
 	ctx4.putImageData(imgData1, 0,0);
 
 	var c5 = document.getElementById("blueCanvas");
 	var ctx5 = c5.getContext("2d");
-	var img2 = document.getElementById("expandedImg");
+	var img2 = document.getElementById("inputImage");
 	ctx5.drawImage(img2, 0, 0);
 	var imgData2 = ctx5.getImageData(0, 0, c5.width, c5.height);
 	// invert colors
 	var i;
 	for (i = 0; i < imgData2.data.length; i += 4) {
-	imgData2.data[i + 2] = 255
+				imgData2.data[i] = 0;
+   				imgData2.data[i+1] = 0 ;
+    			imgData2.data[i+2] = imgData2.data[i + 2];
 	}
 	ctx5.putImageData(imgData2, 0,0);
+	});
+});
 
+$(document).ready(function() {
+	$("#Processing").click(function(){
+	
 
-	/*var c6 = document.getElementById("finalOutput");
-	var ctx6 = c6.getContext("2d");
-	var img6 = document.getElementById("expandedImg");
-	ctx6.drawImage(img6, 0, 0);
-	var imgData6 = ctx6.getImageData(0, 0, c6.width, c6.height);
-	var i;
-	if(slider2.value<0) slider2.value = -1 * slider2.value;
-	for (i = 0; i < imgData6.data.length; i += 4) {
-	imgData6.data[i] = slider2.value;
-	}
-	ctx6.putImageData(imgData6, 0,0);*/
-};
+	
+		var c = document.getElementById("colorSpacingBox");
+		c.style.height = "200px";
+		if(!document.getElementById('HSI').checked && !document.getElementById('CMY').checked && !document.getElementById('YCrCb').checked ){alert("Please Select the Parameters")}
+		else if(document.getElementById("HSI").checked)
+		{
+				$("#parameters1").show();
+				$("#parameters2").hide();
+				$("#parameters3").hide();
+				$("#parameters").show();
+				$("#io").show();
+				$("#ipScript").hide();
+				$("#imagePlanes").hide();
+				$("#linearfunction").hide();
+		}
+		else if (document.getElementById("CMY").checked)
+		{
+				$("#parameters").show();
+				$("#parameters1").hide();
+				$("#parameters2").show();
+				$("#parameters3").hide();
+				$("#io").show();
+				$("#ipScript").hide();
+				$("#imagePlanes").hide();
+				$("#linearfunction").hide();
+		}
+		else 
+		{
+				$("#parameters").show();
+				$("#parameters1").hide();
+				$("#parameters2").hide();
+				$("#parameters3").show();
+				$("#io").show();
+				$("#ipScript").hide();
+				$("#imagePlanes").hide();
+				$("#linearfunction").hide();
+		}
+	});
+});
+$(document).ready(function(){
+	$("#selectImage").click(function() {
+		$("#linearfunction").hide();
+		$("#parameters").hide();
+		$("#io").hide();
+		$("#imagePlanes").show();
+		$("#ipScript").show();
+			var c = document.getElementById("colorSpacingBox");
+			c.style.height = "125px"
 
+	});
+});
+$(document).ready(function(){
+	$("#ColorSpacing").click(function() {
+		$("#parameters").hide();
+		$("#imagePlanes").show();
+		$("#ipScript").show();
+			var c = document.getElementById("colorSpacingBox");
+			c.style.height = "125px";
+	});
+});
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("slope");
 output.innerHTML = slider.value;
 slider.oninput = function() {
-	//reloadDiv();
   output.innerHTML = this.value;
   document.getElementById("eqnslope").innerHTML = slider.value + " r + " + slider2.value;
-   // drawGraph(slider.value,slider2.value);
     
 
 }
@@ -189,61 +154,49 @@ var output2 = document.getElementById("offset");
 output2.innerHTML = slider2.value; 
 
 slider2.oninput = function() {
-	//reloadDiv();
   output2.innerHTML = this.value;
   document.getElementById("eqnslope").innerHTML = slider.value + " r + " + slider2.value;
- // drawGraph(slider.value,slider2.value);
+
   
 }
-
-
-$(document).ready(function() {
-	$("#linear").click(function(){
-		$("#linearfunction").show();
-		//$("#graph").show();
+$(document).ready(function(){
+	$("#linear").click(function() {
+			$("#linearfunction").show();
+			var c = document.getElementById("colorSpacingBox");
+			c.style.height = "400px";
 	});
 });
 
-/*function reloadDiv()
-{ 
-    $( "#graph" ).load(window.location.href + "#graph" );
-}
+$(document).ready(function(){
+	$("#Run").click(function() {
+		if(document.getElementById('HSI').checked || document.getElementById('CMY').checked || document.getElementById('YCrCb').checked)
+			{
+				$("#id").show();
+				$("#instructionBox").hide();
+				$("#instructionBox2").show();
 
-function drawGraph(m, c) {
+			}
+	});
+});
 
-	var graphs = document.getElementById("graph");
-	graphs.style.width = "225px";
-	graphs.style.height = "225px"
-	var graphctx = graphs.getContext('2d');
 
-	var x_coordinate = 225-c/m;
-	var y_coordinate = 225-c;
-
-	graphctx.beginPath();
-
-	graphctx.moveTo(x_coordinate, 0);
-	graphctx.lineTo(0, y_coordinate );
-	graphctx.strokeStyle = "white";
-	graphctx.stroke();
-}*/
 $(document).ready(function() {
-	$("#run").click(function(){
-var c6 = document.getElementById("finalOutput");
+	$("#Run").click(function(){
+	var c6 = document.getElementById("finalOutput");
 	var ctx6 = c6.getContext("2d");
-	var img6 = document.getElementById("expandedImg");
+	var img6 = document.getElementById("inputImage");
 	ctx6.drawImage(img6, 0, 0);
 	var imgData6 = ctx6.getImageData(0, 0, c6.width, c6.height);
-	//ctx6.putImageData(imgData6, 0,0);
-	if(document.getElementById('hsi').checked) {
-			if(document.getElementById('hcr').checked) {
+	if(document.getElementById('HSI').checked) {
+			if(document.getElementById('Hue').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 					for (i = 0; i < imgData6.data.length; i += 4) {
-					imgData6.data[i] = slider2.value;
+					imgData6.data[i + 3] = slider.value;
 					}
 					ctx6.putImageData(imgData6, 0,0);
 			}
-			if(document.getElementById('smg').checked) {
+			if(document.getElementById('Saturation').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 				for (i = 0; i < imgData6.data.length; i += 4) {
@@ -251,7 +204,7 @@ var c6 = document.getElementById("finalOutput");
 				}
 				ctx6.putImageData(imgData6, 0,0);
 			}
-			if(document.getElementById('iyb').checked) {
+			if(document.getElementById('Intensity').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 				for (i = 0; i < imgData6.data.length; i += 4) {
@@ -263,62 +216,66 @@ var c6 = document.getElementById("finalOutput");
 			}
 
 
-
-
-	else if(document.getElementById('cmy').checked) {
-			if(document.getElementById('hcr').checked) {
+	else if(document.getElementById('CMY').checked) {
+			if(document.getElementById('Cyan').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 					for (i = 0; i < imgData6.data.length; i += 4) {
-					imgData6.data[i] = 1- slider2.value;
+					imgData6.data[i + 1] = slider2.value;
+					imgData6.data[i + 2] = slider2.value;
 					}
 					ctx6.putImageData(imgData6, 0,0);
 			}
-			if(document.getElementById('smg').checked) {
+			if(document.getElementById('Magenta').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 				for (i = 0; i < imgData6.data.length; i += 4) {
-				imgData6.data[i + 1] = 1 - slider2.value;
+					imgData6.data[i] = slider2.value;
+					imgData6.data[i + 2] = slider2.value;
 				}
 				//imgData6.data[i + 1] = 1 - imgData6.data[i + 1];
 				ctx6.putImageData(imgData6, 0,0);
 			}
-			if(document.getElementById('iyb').checked) {
+			if(document.getElementById('Yellow').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 				for (i = 0; i < imgData6.data.length; i += 4) {
-				imgData6.data[i + 2] = 1 - slider2.value;
+					imgData6.data[i] = slider2.value;
+					imgData6.data[i + 1] = slider2.value;
 				}	
 				//imgData6.data[i + 2] = 1 - imgData6.data[i + 2];
 				ctx6.putImageData(imgData6, 0,0);
 			}
 	}
-	else if(document.getElementById('rgb').checked) {
-			if(document.getElementById('hcr').checked) {
+	else if(document.getElementById('YCrCb').checked) {
+			if(document.getElementById('Red').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 					for (i = 0; i < imgData6.data.length; i += 4) {
-					imgData6.data[i] = slider2.value;
+					imgData6.data[i] =  slider2.value;
 					}
 					ctx6.putImageData(imgData6, 0,0);
 			}
-			if(document.getElementById('smg').checked) {
+			if(document.getElementById('Green').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 				for (i = 0; i < imgData6.data.length; i += 4) {
-				imgData6.data[i + 1] = slider2.value;
+					imgData6.data[i + 1] = slider2.value;
 				}
 				ctx6.putImageData(imgData6, 0,0);
 			}
-			if(document.getElementById('iyb').checked) {
+			if(document.getElementById('Blue').checked) {
 				var i;
 				if(slider2.value<0) slider2.value = -1 * slider2.value;
 				for (i = 0; i < imgData6.data.length; i += 4) {
-				imgData6.data[i + 2] = slider2.value;
+					imgData6.data[i + 2] = slider2.value;
 				}	
 				ctx6.putImageData(imgData6, 0,0);
 			}
 			
 	}
+
+
 	});
 });
+
